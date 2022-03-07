7df12122bb5a5d23860fc0c695ee6e2ecfbe2789
nto300002 <samonkntd@gmail.co>
2022-03-07 23:13:54 +0900

    menu-button

---
+++ app/core/components/atoms/menu/MenuIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./MenuIcon.module.scss"
+
+const MenuIcon = () => {
+  return (
+    <div className="module-menu-icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
+
+export default MenuIcon
