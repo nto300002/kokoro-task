d2bcd37d4ddc4ecfb717ad1d61c7cbf68a3c6248
nto300002 <samonkntd@gmail.co>
2022-04-20 19:39:16 +0900

    task page syuusei

---
+++ app/pages/task/[taskId].tsx
@@ -0,0 +1,63 @@
+import { Suspense } from "react"
+import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Routes } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import getTask from "app/pages/task/queries/getTask"
+import deleteTask from "app/pages/task/mutations/deleteTask"
+
+export const Task = () => {
+  const router = useRouter()
+  const taskId = useParam("taskId", "number")
+  const [deleteTaskMutation] = useMutation(deleteTask)
+  const [task] = useQuery(getTask, { id: taskId })
+
+  return (
+    <>
+      <Head>
+        <title>Task {task.id}</title>
+      </Head>
+
+      <div>
+        <h1>{task.title}</h1>
+        <pre>{JSON.stringify(task, null, 2)}</pre>
+
+        <Link href={Routes.EditTaskPage({ taskId: task.id })}>
+          <a>編集</a>
+        </Link>
+
+        <button
+          type="button"
+          onClick={async () => {
+            if (window.confirm("削除しますか？")) {
+              await deleteTaskMutation({ id: task.id })
+              router.push(Routes.TasksPage())
+            }
+          }}
+          style={{ marginLeft: "0.5rem" }}
+        >
+          削除
+        </button>
+      </div>
+    </>
+  )
+}
+
+const ShowTaskPage: BlitzPage = () => {
+  return (
+    <div>
+      <p>
+        <Link href={Routes.TasksPage()}>
+          <a>Tasks</a>
+        </Link>
+      </p>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Task />
+      </Suspense>
+    </div>
+  )
+}
+
+ShowTaskPage.authenticate = true
+ShowTaskPage.getLayout = (page) => <Layout>{page}</Layout>
+
+export default ShowTaskPage
--- app/pages/task/index.tsx
+++ app/pages/task/index.tsx
@@ -1,28 +1,68 @@
-import { BlitzPage } from "blitz"
-import Layout from "app/core/layouts/Layout"
-import { GlassmorphismAnalogClock } from "app/core/components/atoms/clock/AnalogClock"
-import TaskList from "app/core/components/organisms/task/TaskList"
 import { Suspense } from "react"
-import { deleteTaskAPI, updateTaskAPI } from "app/mutations/tasks/api"
+import { Head, Link, usePaginatedQuery, useRouter, BlitzPage, Routes, useSession } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import getTasks from "app/pages/task/queries/getTasks"
 
-const Task: BlitzPage = () => {
+const ITEMS_PER_PAGE = 100
+
+export const TasksList = () => {
+  const session = useSession()
+  const router = useRouter()
+  const page = Number(router.query.page) || 0
+  const [{ tasks, hasMore }] = usePaginatedQuery(getTasks, {
+    orderBy: { id: "asc" },
+    skip: ITEMS_PER_PAGE * page,
+    take: ITEMS_PER_PAGE,
+  })
+
+  const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
+  const goToNextPage = () => router.push({ query: { page: page + 1 } })
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <Link href={Routes.ShowTaskPage({ taskId: task.id })}>
+              <a>{task.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <button disabled={page === 0} onClick={goToPreviousPage}>
+        ＜
+      </button>
+      <button disabled={!hasMore} onClick={goToNextPage}>
+        ＞
+      </button>
+    </div>
+  )
+}
+
+const TasksPage: BlitzPage = () => {
   return (
     <>
-      <div className="container">
-        <GlassmorphismAnalogClock />
-        <main>
-          <h1>task</h1>
-          <Suspense fallback={<div>loading...</div>}>
-            {" "}
-            <TaskList deleteTask={deleteTaskAPI} updateTask={updateTaskAPI} />
-          </Suspense>
-        </main>
+      <Head>
+        <title>Tasks</title>
+      </Head>
+
+      <div>
+        <p>
+          <Link href={Routes.NewTaskPage()}>
+            <a>タスクを追加する</a>
+          </Link>
+        </p>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <TasksList />
+        </Suspense>
       </div>
     </>
   )
 }
 
-Task.suppressFirstRenderFlicker = true
-Task.getLayout = (page) => <Layout title="Home">{page}</Layout>
+TasksPage.authenticate = { redirectTo: "/auth/notSignIn" }
+TasksPage.getLayout = (page) => <Layout>{page}</Layout>
 
-export default Task
+export default TasksPage
