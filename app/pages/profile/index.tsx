cca09ee04928c0e49e418f679b596b84b273c09a
nto300002 <samonkntd@gmail.co>
2022-04-20 19:38:51 +0900

    profile page syuusei

--- app/pages/profile/index.tsx
+++ app/pages/profile/index.tsx
@@ -2,12 +2,15 @@ import { BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import styles from "../core/components/auth/authLogin.module.scss"
 import UserProfile from "../../core/components/organisms/profile/UserProfile"
+import { Suspense } from "react"
 
 const Profile: BlitzPage = () => {
   return (
     <div className="container">
       <main>
-        <UserProfile />
+        <Suspense fallback={<div>loading</div>}>
+          <UserProfile />
+        </Suspense>
       </main>
     </div>
   )
