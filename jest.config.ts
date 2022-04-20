f01dfcef4a4e0280156020260aea79ae0471ac23
nto300002 <samonkntd@gmail.co>
2022-04-20 20:53:58 +0900

    kokoro-task

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ export default config
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  modulePaths: ["<rootDir>"],
   preset: "ts-jest",
   testEnvironment: "node",
   globals: {
