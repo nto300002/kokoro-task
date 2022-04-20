9ecc21625e3608e97a7e61749e9f37822a28013d
nto300002 <samonkntd@gmail.co>
2022-04-20 19:36:11 +0900

    Auth mutation syuusei

--- app/mutations/auth/signup.ts
+++ app/mutations/auth/signup.ts
@@ -1,14 +1,15 @@
-import { resolver, SecurePassword } from "blitz"
+import { resolver, SecurePassword, Ctx } from "blitz"
 import db from "db"
-import { Signup } from "app/core/components/auth/validations"
+import { Signup } from "./validations"
 import { Role } from "types"
 
-export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
+export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx: Ctx) => {
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
     data: { email: email.toLowerCase().trim(), hashedPassword, role: "USER", heart: 1 },
     select: { id: true, name: true, email: true, role: true },
   })
+
   await ctx.session.$create({ userId: user.id, role: user.role as Role })
   return user
 })
