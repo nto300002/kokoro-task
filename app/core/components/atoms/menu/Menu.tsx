590524cb75c90954fb0a8b0ec47ba01938d2f833
nto300002 <samonkntd@gmail.co>
2022-05-01 23:50:11 +0900

    セルフモニタリング機能crud frond

--- app/core/components/atoms/menu/Menu.tsx
+++ app/core/components/atoms/menu/Menu.tsx
@@ -23,8 +23,8 @@ const Menu = () => {
             </Link>
           </li>
           <li className={styles.list_li}>
-            <Link href="/music">
-              <a className={styles.list_href}>癒やしの音楽</a>
+            <Link href="/selfmonitoring">
+              <a className={styles.list_href}>セルフモニタリング</a>
             </Link>
           </li>
           <li className={styles.list_li}>
